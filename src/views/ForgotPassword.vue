7dc16f6ec6d172e7f18b200256db6ff6c02e06bb
GreatCode <wef666wef6@gmail.com>
2023-11-11 20:11:26 +0300

    lo

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        
+        <h2>Reset Password</h2>
+        <p>Forgot your password? Enter your email to resset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <img src="../assets/Icons/envelope.png" alt="" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  name: "ForgotPassword",
+};
 </script>
 
 <style lang="scss" scoped>
-
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,15 +1,62 @@
 <template>
-    <div>
+  <div class="form-wrap">
+    <form class="register">
+      <p class="login-register">
+        Already have an account?
+        <router-link class="router-link" :to="{ name: 'login' }">
+          Login
+        </router-link>
+      </p>
+      <h2>Create Your FireBlog Account</h2>
+      <div class="inputs">
+        <div class="input">
+          <input type="text" placeholder="First Name" v-model="firstName" />
+          <img src="../assets/Icons/user.jpg" alt="" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Last Name" v-model="lastName" />
+          <img src="../assets/Icons/user.jpg" alt="" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Username" v-model="userName" />
+          <img src="../assets/Icons/user.jpg" alt="" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Email" v-model="email" />
+          <img src="../assets/Icons/envelope.png" alt="" />
+        </div>
+        <div class="input">
+          <input type="password" placeholder="Password" v-model="password" />
+          <img src="../assets/Icons/lock.jpg" alt="" />
+        </div>
+      </div>
 
-    </div>
+      <button>Sign Up</button>
+      <div class="angle"></div>
+    </form>
+    <div class="background"></div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  name: "Register",
+  data() {
+    return {
+      firstName: null,
+      lastName: null,
+      userName: null,
+      email: null,
+      password: null,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.register {
+  h2 {
+    max-width: 350px;
+  }
+}
 </style>
